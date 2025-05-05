a96a5a536458d63b70ec0de5f9502183d412d99b
duvarakeshss <duvarakesh05@gmail.com>
2025-05-05 11:59:36 +0530

    commit

--- client/src/pages/homePage.jsx
+++ client/src/pages/homePage.jsx
@@ -10,7 +10,7 @@ const Home = () => {
   const [greeting, setGreeting] = useState('')
   const [attendanceData, setAttendanceData] = useState([])
   const [error, setError] = useState('')
-  const [customPercentage, setCustomPercentage] = useState(75)
+  const [customPercentage, setCustomPercentage] = useState(80)
   const [combinedData, setCombinedData] = useState([])
 
   // Prevent going back to login page when back button is clicked
